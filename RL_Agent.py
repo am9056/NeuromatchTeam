5b0a255ccc2ff119c2dd2112406b8a3b8c0383bb
Andrew Mah <am9056@nyu.edu>
2020-07-21 11:49:25 -0400

    Added Kyle's code

--- RL_Agent.py
+++ RL_Agent.py
@@ -3,8 +3,8 @@ import numpy as np
 import matplotlib.pyplot as plt
 import os, requests
 
-#%%
-#@title Data retrieval
+#%% Download Data
+
 fname = []
 for j in range(3):
   fname.append('steinmetz_part%d.npz'%j)
@@ -35,6 +35,7 @@ print(dat.keys())
 
 #%%
 
+
 # Rascola-Wagner Model of Value
 def value(r, alpha):
     v = np.full(r.shape, np.nan)
@@ -45,13 +46,71 @@ def value(r, alpha):
 
     return v
 
+# R = np.ones((1, 1000))[0]
+R = dat['feedback_type']
 
-V = value(dat['feedback_type'], 0.1)
-plt.plot(dat['feedback_type'], 'r--')
+V = value(R, 0.1)
+plt.plot(R, 'r--')
 plt.plot(V)
 
 plt.show()
 
 #%%
+total_contrasts = dat["contrast_left"] - dat["contrast_right"]
+gotrials = np.logical_and(dat["contrast_left"]!=0, dat["contrast_right"]!=0)
+go_contrasts = total_contrasts[gotrials]
 
+ntrials = len(go_contrasts)
 
+beta = 30.
+
+prob_left = np.exp(beta*go_contrasts)/\
+            (np.exp(-beta*go_contrasts)[0] + np.exp(beta*go_contrasts))
+
+response = dat["response"][gotrials]
+response[response == -1] = 0
+plt.plot(response)
+plt.plot(prob_left)
+plt.show()
+
+#%%
+
+print(dat['feedback_type'].shape)
+plt.plot(dat['feedback_type'])
+
+mean_smoothed_response = my_moving_window(dat['feedback_type'], window = 20)
+plt.plot(mean_smoothed_response)
+
+x_points, gamma = np.arange(0,20,1), .1
+
+exp_function = gamma* np.exp(-gamma * x_points)
+
+value = np.convolve(exp_function,dat['feedback_type'],mode = 'same')
+plt.plot(value)
+# plt.figure
+# plt.plot(exp_function)
+
+#%%
+
+idx = np.logical_and(dat['contrast_left'] != 0 , dat['contrast_right'] != 0)
+# print(idx)
+total_contrast = dat['contrast_left'][idx] - dat['contrast_right'][idx]
+total_response = dat['response'][idx]
+noisy_response = total_response + 0.2 * np.random.rand(len(total_response))
+plt.scatter(total_contrast,noisy_response)
+
+contrasts = np.unique(total_contrast)
+mean_response = np.zeros_like(contrasts)
+for i,c in enumerate(contrasts):
+  idx2 = total_contrast == c
+  mean_response[i] = np.mean(total_response[idx2])
+
+plt.plot(contrasts, mean_response)
+
+#%%
+
+# Softmax Function
+fcv = total_contrast * value[idx]
+beta = 3
+PCL = np.exp(beta*fcv)/ (np.exp(-beta*fcv) + np.exp(beta*fcv))
+plt.scatter(total_contrast,PCL)
